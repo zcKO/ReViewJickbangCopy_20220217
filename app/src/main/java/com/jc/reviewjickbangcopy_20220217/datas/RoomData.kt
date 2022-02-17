cd7ee938538a51d2f7fbbfba17e0d12ef2ec39e5
JaeCheon <studyJcHub@gmail.com>
2022-02-17 17:56:46 +0900

    RoomData 클래스 작성

---
+++ app/src/main/java/com/jc/reviewjickbangcopy_20220217/datas/RoomData.kt
@@ -0,0 +1,14 @@
+package com.jc.reviewjickbangcopy_20220217.datas
+
+/**
+ * @param price 방 가격
+ * @param address 방 주소
+ * @param floor 방의 층 수
+ * @param description 방의 설명
+ */
+class RoomData(
+    val price: Int,
+    val address: String,
+    val floor: Int,
+    val description: String
+)
