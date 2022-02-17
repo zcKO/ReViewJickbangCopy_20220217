f75eebe430002c0a2483d8e30dfa3abb4820c563
JaeCheon <studyJcHub@gmail.com>
2022-02-17 18:08:35 +0900

    룸 정보 액티비티 생성

---
+++ app/src/main/java/com/jc/reviewjickbangcopy_20220217/RoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.jc.reviewjickbangcopy_20220217
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_detail)
+    }
+}
